0d1fc44a8918b901ea054dd996e78e3c58914ed0
QJWIN <QJWIN@users.noreply.github.com>
2026-05-24 20:13:11 +0200

    Release website 0.5.3 crash fixes

--- emce-updates-worker.js
+++ emce-updates-worker.js
@@ -11,9 +11,9 @@ export default {
     }
 
     return json({
-      latestVersion: "0.5.1",
+      latestVersion: "0.5.3",
       minecraftVersion: "1.21.11",
-      downloadUrl: "https://qjwin.github.io/EMCEssentials/downloads/EMCEssentials-0.5.1.jar",
+      downloadUrl: "https://qjwin.github.io/EMCEssentials/downloads/EMCEssentials-0.5.3.jar",
       changelogUrl: "https://qjwin.github.io/EMCEssentials/changelog.html"
     });
   }
