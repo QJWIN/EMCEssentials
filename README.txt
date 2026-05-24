55fcee2c908de2735511f246bafba749d0fa31bb
QJWIN <QJWIN@users.noreply.github.com>
2026-05-24 17:56:22 +0200

    Release website for EMCE 0.5.0

---
+++ README.txt
@@ -0,0 +1,1 @@
+no set yet
---
+++ emce-updates-worker.js
@@ -0,0 +1,31 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    if (request.method !== "GET") {
+      return json({ error: "Method not allowed" }, 405);
+    }
+
+    if (url.pathname !== "/emce/latest") {
+      return json({ error: "Not found" }, 404);
+    }
+
+    return json({
+      latestVersion: "0.5.0",
+      minecraftVersion: "1.21.11",
+      downloadUrl: "https://qjwin.github.io/EMCEssentials/downloads/EMCEssentials-0.5.0.jar",
+      changelogUrl: "https://qjwin.github.io/EMCEssentials/changelog.html"
+    });
+  }
+};
+
+function json(body, status = 200) {
+  return new Response(JSON.stringify(body, null, 2), {
+    status,
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "cache-control": "no-store",
+      "access-control-allow-origin": "*"
+    }
+  });
+}
